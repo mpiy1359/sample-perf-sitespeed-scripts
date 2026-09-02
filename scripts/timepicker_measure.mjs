dd4611777883b9b94afff0979400a8c504b79cda
Maleesh Piyaratne <maleesh.piyaratne@syscolabs.com>
2026-09-02 07:49:18 +0000

    Update timepicker_measure.mjs

--- scripts/timepicker_measure.mjs
+++ scripts/timepicker_measure.mjs
@@ -6,11 +6,13 @@ export default async function (context, commands) {
     commands.meta.setTitle('Test Grafana SPA');
     commands.meta.setDescription('Test the first page, click the timepicker and then choose <b>Last 30 days</b> and measure that page.');
     await commands.measure.start(
-        'https://dashboard.sitespeed.io/d/000000044/page-timing-metrics?orgId=1','pageTimingMetricsDefault'
+        'https://dashboard.sitespeed.io/d/000000044/page-timing-metrics?orgId=1', 'pageTimingMetricsDefault'
     );
     await commands.click('class:gf-timepicker-nav-btn');
     await commands.wait.byTime(1000);
     await commands.measure.start('pageTimingMetrics30Days');
-    await commands.click('link:Last 30 days', { waitForNavigation: true });
+    await commands.click('link:Last 30 days', {waitForNavigation: true});
     await commands.measure.stop();
 };
+
+// EXECUTION COMMAND: npx sitespeed.io.cmd timepicker_measure.mjs -n 1
